71faf41c7a02f531ca626e18d6fcf56b881bc746
Sebastian Camilo Vanegas Ayala <scva9708@gmail.com>
2025-09-09 15:08:01 -0500

    Singleton sin funcionar

---
+++ src/main/java/udmodelos20253/singletoncodex/SingletonCodex.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package udmodelos20253.singletoncodex;
+
+/**
+ *
+ * @author SCVA9
+ */
+public class SingletonCodex {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
